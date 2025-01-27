09d6c422b310eed15fe95879e017fc214214083a
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2025-01-27 15:38:48 +0500

    server

--- backend/server.js
+++ backend/server.js
@@ -82,6 +82,8 @@ app.use(
   cors({
     origin: ["https://hackaton-ayun.vercel.app", "http://localhost:5173", "http://localhost:3000"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 )
 app.use(express.json())
